bb3de9cdcd7fc839e0a07e16a4ea82e99e0dff38
iain <iain@soulflyer.co.uk>
2018-11-26 20:09:39 +0700

    Initial commit

---
+++ pymidi13.framework/Versions/A/Headers/pymidi13.h
@@ -0,0 +1,19 @@
+//
+//  pymidi13.h
+//  pymidi13
+//
+//  Created by Iain Wood on 25/11/2018.
+//  Copyright © 2018 Soulflyer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for pymidi13.
+FOUNDATION_EXPORT double pymidi13VersionNumber;
+
+//! Project version string for pymidi13.
+FOUNDATION_EXPORT const unsigned char pymidi13VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pymidi13/PublicHeader.h>
+
+
